53e424cad89184038d46ed9d5a63cd96887b8065
danniromedanni <127797688+danniromedanni@users.noreply.github.com>
2024-01-22 15:22:04 -0300

    4 commit
    eventos

--- RomeCode Proyect/eventos.js
+++
@@ -1,8 +0,0 @@
-function moverCard(elemento) {
-    elemento.style.transform = "translateY(-20px)"; // Puedes ajustar la cantidad de movimiento aquí
-    elemento.style.transition = "transform 0.3s ease";
-}
-
-function restaurarCard(elemento) {
-    elemento.style.transform = "translateY(0)";
-}
